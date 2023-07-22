b88716372cd953ae184d1e635b302c86edd68f0c
mrProger <ilya.kargapolov02@mail.ru>
2023-07-23 00:17:58 +0600

    Реализована авторизация по токену

--- layers/Interactor/route/route_load_all_news.py
+++ layers/Interactor/route/route_load_all_news.py
@@ -1,11 +1,11 @@
 from rest_framework.views import APIView
 from layers.Interactor.usecase.load_all_news.get.us_get_load_all_news import us_get_load_all_news
 from django.contrib.auth.models import User
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 class RouteLoadAllNews(APIView):
-    authentication_classes = [SessionAuthentication]
+    authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
 
     def get(self, request):
