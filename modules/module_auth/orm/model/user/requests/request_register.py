b4168ccb41ffe9a89d55be6df542dc8c1bcd3511
Ilya_Kargapolov <ilya.kargapolov02@mail.ru>
2023-08-24 17:16:16 +0600

    fix

--- modules/module_auth/orm/model/user/requests/request_register.py
+++ modules/module_auth/orm/model/user/requests/request_register.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 
 class RequestRegister:
     def execute(username, password):
-        user = User(username=request.data.get('username'), password=request.data.get('password'))
+        user = User(username=username, password=password)
         user.is_staff = True
         user.save()
         return user
